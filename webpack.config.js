e35cd8887c99f366247f85e64ec5805b087ec86f
Michelle Huang <michelleahuang@berkeley.edu>
2022-03-11 09:53:51 -0800

    Update webpack

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+
+module.exports = {
+    entry: './frontend/culture_biosciences.jsx',
+    output: {
+        path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
+        filename: 'bundle.js'
+    },
+    resolve: {
+        extensions: [".js", ".jsx", "*"]
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                exclude: /(node_modules)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/env', '@babel/react']
+                    }
+                }
+            },
+            {
+                test: /\.(png|jpg|svg|gif|mp3)$/,
+                type: 'asset/resource'
+            }
+        ]
+    },
+    devtool: 'source-map'
+}
