8949e0369b616c1d51df70358f722fa3205cf26d
Michelle Huang <michelleahuang@berkeley.edu>
2022-03-15 11:30:48 -0700

    Working on pagination

--- frontend/components/images/images_index.jsx
+++ frontend/components/images/images_index.jsx
@@ -1,15 +1,22 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 
 function ImagesIndex(props) {
+
+    const [images, setImages] = useState([]);
+    const [currentPage, setCurrentPage] = useState(1);
+    const [imagesPerPage, setImagesPerPage] = useState(100);
 
     useEffect(() => {
         props.fetchAllImages();
     }, [])
 
-    return (
+    const indexOfLastImage = currentPage * imagesPerPage;
+    const indexOfFirstImage = indexOfLastImage - imagesPerPage;
+    const currentImages = props.images.slice(indexOfFirstImage, indexOfLastImage);
 
+    return (
         <ul>
-            {props.images.map((image, idx) => {
+            {currentImages.map((image, idx) => {
                 return <li key={idx}>{image.url}</li>
             })}
         </ul>
